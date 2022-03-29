80c6b90c9bdaf00061c68a183b558f7bb372503d
Minji Kong <mkong16@my.bcit.ca>
2022-03-29 11:16:25 -0700

    hosting

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyBGI_GNUls6P-CfKQiG-qfJKZa2WCrcJN8",
+    authDomain: "fir-comp1800-e96cb.firebaseapp.com",
+    projectId: "fir-comp1800-e96cb",
+    storageBucket: "fir-comp1800-e96cb.appspot.com",
+    messagingSenderId: "227872544634",
+    appId: "1:227872544634:web:9e2fe7d236c926d6df68c3"
 };
 
 //--------------------------------------------
